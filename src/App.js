d9c8daf7f022de27721ec9ffad377ecfc5a4098e
jsilv21 <johnksilverwood@gmail.com>
2022-03-27 15:27:00 -0400

    initialize

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import {React, useState, useRef, useEffect} from "react";
+import TodoList from "./TodoList";
+import uuidv4 from '../node_modules/uuid/dist/v4.js'
+
+//setup local storage
+const LOCAL_STORAGE_KEY = 'todoApp.todos';
 
 function App() {
+  const [todos, setTodos] = useState([])
+  //getting name of todo in field
+  const todoNameRef = useRef();
+
+  //load todos
+  useEffect(()=>{
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (storedTodos) setTodos(storedTodos)
+  },[])
+
+  //store todos
+  useEffect(()=> {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  }, [todos])
+
+  function toggleToDo(id){
+    //create copy to modify
+    const newTodos = [...todos]
+    //match ID's to update
+    const todo = newTodos.find(todo => todo.id ===id)
+    todo.complete = !todo.complete //toggle it
+    setTodos(newTodos) //pass em back
+  }
+
+  function handleAddTodo(e){
+    const name = todoNameRef.current.value
+    if (name === '') return
+    //setting todos, pulling in any previous from storage with prevTodos array.
+    setTodos(prevTodos => {
+      return [...prevTodos, {id: uuidv4() //uuid for uniqueness
+        , name: name, complete: false}]
+    })
+    todoNameRef.current.value = null;
+  }
+
+  function handleClearTodos(){
+    //grab all non complete and overwrite list to that (removing completes)
+    const newTodos = todos.filter(todo=> !todo.complete)
+    setTodos(newTodos);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>  
+    <p>The most basic react app ever 💪</p>
+    <TodoList todos={todos} toggleToDo={toggleToDo} />
+    <input ref={todoNameRef} type="text"></input>
+    <button onClick={handleAddTodo}>Add Item</button>
+    <button onClick={handleClearTodos}>Clear Complete</button>
+    <div>{todos.filter(todo => !todo.complete).length} left to do</div>
+    <br></br>
+    <div> <a href='https://www.youtube.com/watch?v=hQAHSlTtcmY'>Courtesy of web dev simplified</a></div>
+    </>
+  )
 }
 
 export default App;
